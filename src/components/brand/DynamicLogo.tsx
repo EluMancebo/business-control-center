f55e6975faff9278ebcc3ccde71edb5edbb5a2ad
EluM <emango0298@gmail.com>
2026-02-14 01:07:40 +0100

    feat(brand): add BCC brand assets and logo components

---
+++ src/components/brand/DynamicLogo.tsx
@@ -0,0 +1,57 @@
+// src/components/brand/DynamicLogo.tsx
+import type { HTMLAttributes } from "react";
+
+type LogoSize = 28 | 32 | 36 | 40 | 48;
+
+type DynamicLogoProps = {
+  size?: LogoSize;
+  animated?: boolean;
+  label?: string;
+} & HTMLAttributes<HTMLDivElement>;
+
+function sizeClass(size: LogoSize) {
+  switch (size) {
+    case 28:
+      return "h-7 w-7";
+    case 32:
+      return "h-8 w-8";
+    case 36:
+      return "h-9 w-9";
+    case 40:
+      return "h-10 w-10";
+    case 48:
+      return "h-12 w-12";
+    default:
+      return "h-9 w-9";
+  }
+}
+
+export default function DynamicLogo({
+  size = 36,
+  animated = true,
+  label = "Business Control Center",
+  className = "",
+  ...rest
+}: DynamicLogoProps) {
+  const box = sizeClass(size);
+
+  return (
+    <div
+      {...rest}
+      className={`relative inline-flex ${box} items-center justify-center ${className}`}
+      aria-label={label}
+      title={label}
+    >
+      {/* Logo mark real */}
+      <img
+        src="/brand/logo-mark.svg"
+        alt={label}
+        className={[
+          "relative h-full w-full",
+          animated ? "animate-[float_5s_ease-in-out_infinite]" : "",
+        ].join(" ")}
+        draggable={false}
+      />
+    </div>
+  );
+}
